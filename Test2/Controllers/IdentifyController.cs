daa92cce3500e2c1208c11073daa95e8bd4e583f
EvaKuo <s8902151@gmail.com>
2024-04-10 22:19:53 +0800

    test

---
+++ Test2/Controllers/IdentifyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestCode1.Controllers
+{
+    public class IdentifyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Test2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestCode1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
