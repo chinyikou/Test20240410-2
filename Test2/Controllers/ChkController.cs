0c350c4c31268c7505e63b51e5ee52b491a5864d
EvaKuo <s8902151@gmail.com>
2024-04-10 22:59:45 +0800

    controller add

---
+++ Test2/Controllers/ChkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestCode1.Controllers
+{
+    public class ChkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
